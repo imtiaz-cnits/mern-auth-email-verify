f67437ca408f7f755cf4535b17b02631ee7b5cc0
imtiaz-cnits <ia64744@gmail.com>
2025-05-27 11:48:20 +0600

    home work

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,17 +1,9 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-      mongoose.connection.on('connected', () => console.log("Database connected successfully"));
-    const connectDB = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  mongoose.connection.on('Connected', () => console.log('Database Connected'));
+
+  await mongoose.connect(`${process.env.MONGO_URI}/MERNAUTH`);
 }
 
 export default connectDB;

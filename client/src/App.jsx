b547727b7cc489f5adf4d66c3690381da3ff1bfd
imtiaz-cnits <ia64744@gmail.com>
2025-06-03 04:22:53 +0600

    frontend started

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='text-xl text-blue-500 font-bold'>
+            Hello
+        </div>
+    );
+};
+
+export default App;
